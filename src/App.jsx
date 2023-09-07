d91e432bea8b04e050d3d9ff82aab37efed5f8b0
unknown <nazrulislam66188@gmail.com>
2023-09-07 12:37:27 +0600

    topbar created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css'
 import { ColorModeContext, useMode } from './theme'
 import Topbar from './scenes/global/Topbar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './scenes/dashboard';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,8 +13,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
-          <main className='context'>
+          <main className='content'>
             <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,48 @@
+import { Box, IconButton, InputBase, useTheme } from "@mui/material"
+import { ColorModeContext, tokens } from "../../theme";
+import { useContext } from "react";
+import { DarkModeOutlined, LightModeOutlined, NotificationsOutlined, PersonOutlineOutlined, Search, SettingsOutlined } from "@mui/icons-material";
+
 
 const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
     return (
-        <div>Topbar</div>
+        <Box display="flex" justifyContent="space-between" p={2} >
+            {/* search bar */}
+            <Box display='flex' backgroundColor={colors.primary[400]} borderRadius='8px'>
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: 1 }}>
+                    <Search />
+                </IconButton>
+            </Box>
+
+            {/* icons */}
+            <Box display='flex'>
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {
+                        theme.palette.mode === 'dark' ? (
+                            <DarkModeOutlined />
+                        )
+                            : (
+                                <LightModeOutlined />
+                            )
+                    }
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlined />
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined />
+                </IconButton>
+                <IconButton>
+                    <PersonOutlineOutlined />
+                </IconButton>
+
+            </Box>
+        </Box>
     )
 }
 
